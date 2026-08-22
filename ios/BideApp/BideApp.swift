b2844d6008e854f018a38194dcfd863d6dafbe1b
Frederick "Trey" Barfield <fbarfiel@gmu.edu>
2026-08-22 14:08:13 -0400

    Persist native workspace across app lifecycle

--- ios/BideApp/BideApp.swift
+++ ios/BideApp/BideApp.swift
@@ -3,11 +3,16 @@ import SwiftUI
 @main
 struct BideApp: App {
     @StateObject private var session = AppSession()
+    @StateObject private var workspace = WorkspaceStore()
 
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environmentObject(session)
+                .environmentObject(workspace)
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification)) { _ in
+                    workspace.saveActiveDocumentNow()
+                }
         }
     }
 }
