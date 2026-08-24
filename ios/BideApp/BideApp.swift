47b42c9a252d34d35ed32ebb296cb2e6da84ac57
Frederick "Trey" Barfield <fbarfiel@gmu.edu>
2026-08-24 14:06:08 -0700

    Recover unfinished saved results before dataset reconciliation

--- ios/BideApp/BideApp.swift
+++ ios/BideApp/BideApp.swift
@@ -54,6 +54,7 @@ struct BideApp: App {
         guard let projectID else { return }
         Task {
             guard dataWorkspace.recoverInterruptedDatasetDeletions(projectID: projectID) else { return }
+            guard dataWorkspace.recoverInterruptedSavedResults(projectID: projectID) else { return }
             await dataWorkspace.reconcileProjectFiles(projectID: projectID)
             await dataWorkspace.migrateDerivedDatabaseIfNeeded(projectID: projectID)
         }
